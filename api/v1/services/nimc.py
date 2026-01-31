1c9d8eeeaa200970d59d2c166106835b16915e06
Adegbehingbe Oluwakorede Joseph <oluwakorede@Adegbehingbes-MacBook-Pro.local>
2026-02-01 00:40:03 +0100

    minor updates

--- api/v1/services/nimc.py
+++ api/v1/services/nimc.py
@@ -1,45 +1,50 @@
-from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel
 from cryptography.hazmat.primitives.asymmetric import ec
-from cryptography.hazmat.primitives import serialization, hashes
+from cryptography.hazmat.primitives import hashes, serialization
 from cryptography.hazmat.primitives.kdf.hkdf import HKDF
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.backends import default_backend
-import ipfshttpclient
-import hashlib
 import os
 import json
-import requests
 from base64 import b64encode, b64decode
-
-from sqlalchemy.orm import Session
-
-app = FastAPI(title="Blockchain Identity PoC")
 
 # -------------------------
 # ECC KEY PAIR (PoC ONLY)
 # -------------------------
-private_key = ec.generate_private_key(ec.SECP256R1())
-public_key = private_key.public_key()
+# private_key = ec.generate_private_key(ec.SECP256R1())
+# public_key = private_key.public_key()
+
+KEY_FILE = "ecc_private.pem"
 
 
 class NIMCService:
     
+    @staticmethod
+    def _get_key():
+        if os.path.exists(KEY_FILE):
+            return serialization.load_pem_private_key(
+                open(KEY_FILE, "rb").read(),
+                password=None
+            )
+
+        key = ec.generate_private_key(ec.SECP256R1())
+        with open(KEY_FILE, "wb") as f:
+            f.write(
+                key.private_bytes(
+                    encoding=serialization.Encoding.PEM,
+                    format=serialization.PrivateFormat.PKCS8,
+                    encryption_algorithm=serialization.NoEncryption()
+                )
+            )
+        return key
+
+
     @classmethod
     def encrypt(cls, data: dict):
-        shared_key = private_key.exchange(ec.ECDH(), public_key)
-
-        derived_key = HKDF(
-            algorithm=hashes.SHA256(),
-            length=32,
-            salt=b"static-salt",
-            info=b'identity-demo',
-            backend=default_backend()
-        ).derive(shared_key)
-
+        key = cls._get_key()
         iv = os.urandom(12)
+
         encryptor = Cipher(
-            algorithms.AES(derived_key),
+            algorithms.AES(key),
             modes.GCM(iv),
             backend=default_backend()
         ).encryptor()
@@ -53,21 +58,13 @@ class NIMCService:
             "tag": b64encode(encryptor.tag).decode()
         }
         
-    
+
     @classmethod
     def decrypt(cls, enc: dict):
-        shared_key = private_key.exchange(ec.ECDH(), public_key)
-
-        derived_key = HKDF(
-            algorithm=hashes.SHA256(),
-            length=32,
-            salt=b"static-salt",
-            info=b'identity-demo',
-            backend=default_backend()
-        ).derive(shared_key)
+        key = cls._get_key()
 
         decryptor = Cipher(
-            algorithms.AES(derived_key),
+            algorithms.AES(key),
             modes.GCM(
                 b64decode(enc["iv"]),
                 b64decode(enc["tag"])
@@ -75,5 +72,62 @@ class NIMCService:
             backend=default_backend()
         ).decryptor()
 
-        plaintext = decryptor.update(b64decode(enc["ciphertext"])) + decryptor.finalize()
+        plaintext = decryptor.update(
+            b64decode(enc["ciphertext"])
+        ) + decryptor.finalize()
+        
         return json.loads(plaintext)
+
+    
+    # @classmethod
+    # def encrypt(cls, data: dict):
+    #     shared_key = private_key.exchange(ec.ECDH(), public_key)
+
+    #     derived_key = HKDF(
+    #         algorithm=hashes.SHA256(),
+    #         length=32,
+    #         salt=b"static-salt",
+    #         info=b'identity-demo',
+    #         backend=default_backend()
+    #     ).derive(shared_key)
+
+    #     iv = os.urandom(12)
+    #     encryptor = Cipher(
+    #         algorithms.AES(derived_key),
+    #         modes.GCM(iv),
+    #         backend=default_backend()
+    #     ).encryptor()
+
+    #     plaintext = json.dumps(data).encode()
+    #     ciphertext = encryptor.update(plaintext) + encryptor.finalize()
+
+    #     return {
+    #         "ciphertext": b64encode(ciphertext).decode(),
+    #         "iv": b64encode(iv).decode(),
+    #         "tag": b64encode(encryptor.tag).decode()
+    #     }
+        
+    
+    # @classmethod
+    # def decrypt(cls, enc: dict):
+    #     shared_key = private_key.exchange(ec.ECDH(), public_key)
+
+    #     derived_key = HKDF(
+    #         algorithm=hashes.SHA256(),
+    #         length=32,
+    #         salt=b"static-salt",
+    #         info=b'identity-demo',
+    #         backend=default_backend()
+    #     ).derive(shared_key)
+
+    #     decryptor = Cipher(
+    #         algorithms.AES(derived_key),
+    #         modes.GCM(
+    #             b64decode(enc["iv"]),
+    #             b64decode(enc["tag"])
+    #         ),
+    #         backend=default_backend()
+    #     ).decryptor()
+
+    #     plaintext = decryptor.update(b64decode(enc["ciphertext"])) + decryptor.finalize()
+        # return json.loads(plaintext)
